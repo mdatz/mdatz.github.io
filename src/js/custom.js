acb36bf31cb61fcd0a504664a8fba92dfd607fb7
mdatz <kadatzmatthew@gmail.com>
2020-09-12 02:07:02 -0600

    Added Simple Anime.js animations to container elements

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,16 +1,63 @@
 function display_Home(){
+	
+	var incoming = document.querySelectorAll('#main-text');
+
+	var timeline = anime.timeline({
+		easing: 'easeOutExpo',
+		duration: 750
+	});
+
+	let animation_in = anime({
+		targets: incoming,
+		translateY:[300,0],
+		duration: 750,
+	});
+
+	timeline.add(animation_in);
+
 	document.getElementById("main-text").style.display = "block";
 	document.getElementById("container-A").style.display = "none";
 	document.getElementById("container-B").style.display = "none";
 }
 
 function display_About(){
+
+	var incoming = document.querySelectorAll('#container-A');
+
+	var timeline = anime.timeline({
+		easing: 'easeOutExpo',
+		duration: 750
+	});
+
+	let animation_in = anime({
+		targets: incoming,
+		translateY:[300,0],
+		duration: 750,
+	});
+
+	timeline.add(animation_in);
+
 	document.getElementById("main-text").style.display = "none";
 	document.getElementById("container-A").style.display = "block";
 	document.getElementById("container-B").style.display = "none";
 }
 
 function display_Contact(){
+
+	var incoming = document.querySelectorAll('#container-B');
+
+	var timeline = anime.timeline({
+		easing: 'easeOutExpo',
+		duration: 750
+	});
+
+	let animation_in = anime({
+		targets: incoming,
+		translateY:[300,0],
+		duration: 750,
+	});
+
+	timeline.add(animation_in);
 
 	document.getElementById("main-text").style.display = "none";
 	document.getElementById("container-A").style.display = "none";
